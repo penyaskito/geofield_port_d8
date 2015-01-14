9fa1193fd00fe922271cdcb9dc67ce31b28dbe67
Christian Lopez Espinola <penyaskito@computer.org>
2015-01-14 13:28:23 +0100

    Attaching css is not allowed. Created geofield.proximity_element library.

--- src/Element/GeofieldProximity.php
+++ src/Element/GeofieldProximity.php
@@ -52,7 +52,7 @@ class GeofieldProximity extends FormElement {
   public static function proximityProcess(&$element, FormStateInterface $form_state, &$complete_form) {
     $element['#attributes'] = array('class' => array('clearfix'));
     $element['#tree'] = TRUE;
-    $element['#attached']['css'] = array(drupal_get_path('module', 'geofield') . '/css/proximity-element.css');
+    $element['#attached']['library'][] = 'geofield/geofield.proximity_element';
 
     // Create the textfield for distance.
     $element['distance'] = array(
